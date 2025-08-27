991857e934ea4459956d75bd7ffd48c1a2a0d199
PRATHAM N GUNDIKERE <prathamgundikere@gmail.com>
2025-08-28 01:47:32 +0530

    Default Dark Theme

--- assets/javascript/toggleDark.js
+++ assets/javascript/toggleDark.js
@@ -1,12 +1,19 @@
 var checkbox = document.querySelector('input[name=mode]')
+var moon = document.getElementById('moon')
+
+// Force dark mode on first load
+document.documentElement.setAttribute('data-theme', 'dark')
+checkbox.checked = true
+moon.innerHTML = feather.icons['sun'].toSvg({'fill': 'var(--fg-color-dark)'})
+
+// Add toggle listener
 checkbox.addEventListener('change', function() {
     if(this.checked) {
         document.documentElement.setAttribute('data-theme', 'dark')
-        var moon = document.getElementById('moon')
         moon.innerHTML = feather.icons['sun'].toSvg({'fill': 'var(--fg-color-dark)'})
     } else {
         document.documentElement.setAttribute('data-theme', 'light')
-        var moon = document.getElementById('moon')
         moon.innerHTML = feather.icons['moon'].toSvg({'fill': 'var(--fg-color-dark)'})
     }
 })
+
